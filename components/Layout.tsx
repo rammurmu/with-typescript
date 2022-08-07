7f26c1def3850eab9487e1a3c3e26082d157e21b
Ram Murmu <runashInc@outlook.com>
2022-08-07 16:09:11 +0530

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
     {children}
     <footer>
       <hr />
-      <span>I'm here to stay (Footer)</span>
+      <span>Written in India for Worlds ,We make something people want</span>
     </footer>
   </div>
 )
